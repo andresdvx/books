878d145bb472e63c1804921fceb1d075d1e62e00
andresdvx <andresdavidpachecocuadro@gmail.com>
2024-08-30 20:04:48 -0500

    fix: bug middleware if body request is empty thorw exception

--- src/common/middlewares/book.middleware.ts
+++ src/common/middlewares/book.middleware.ts
@@ -1,11 +1,14 @@
 import { Request, Response, NextFunction } from "express";
 import { BadRequestError, InternalServerError } from "../http/index";
 import { Schema } from "joi";
+import { Book } from "@prisma/client";
 
 export const bookValidator =
   (schema: Schema) => (req: Request, res: Response, next: NextFunction) => {
     try {
-      const body = req.body;
+      const body : Partial<Book> = req.body;
+      if(!body || Object.keys(body).length === 0) throw new BadRequestError('body request cannot be empty');
+      
       const { error, value } = schema.validate(body);
       if (error?.message) {
         throw new BadRequestError(error.message);
